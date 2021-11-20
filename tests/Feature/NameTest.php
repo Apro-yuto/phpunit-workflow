aa280cb8efcf8613492fd24be2d71385250d36f8
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2021-11-20 13:19:53 +0900

    Fix: 異常系→正常系

--- tests/Feature/NameTest.php
+++ tests/Feature/NameTest.php
@@ -2,36 +2,37 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use App\Models\Name;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class NameTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
      * A basic feature test index.
      *
      * @return void
      */
-    public function test_index()
+    public function testIndex()
     {
         $response = $this->get('/')
                         ->assertStatus(200);
     }
+
     /**
      * A basic feature test index.
      *
      * @return void
      */
-    public function test_postRegister()
+    public function testPostRegister()
     {
         $test = Name::factory()->create();
 
         $this->assertDatabaseHas('names', [
-            'last_name' => $test->last_name,
-            'first_name' => $test->firstName,
+            'last_name'  => $test->last_name,
+            'first_name' => $test->first_name,
         ]);
     }
 }
